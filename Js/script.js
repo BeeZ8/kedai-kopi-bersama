5205ebe0fc55aa1bf5666ea8b685adf2c86f313d
Ravi <kickrush87@gmail.com>
2024-11-29 17:22:37 +0700

    membuat website kedia serba kuliner

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,51 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarnav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarnav.classList.toggle("active");
 };
 
-// klik di luar slidebar untuk menghilangkan nav
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-const hamburger = document.querySelector("#hamburger-menu");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle class active untuk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar elemen
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
     navbarnav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+//modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelector(".item-detail-button");
+
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
